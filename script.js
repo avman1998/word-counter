2fc5f57ccf9b0ee9599376b6ced96015df323dc9
Your Name <you@example.com>
2022-10-02 00:29:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const textareaEle = document.querySelector(".main-container #myText");
+const wordResult = document.querySelector(".main-container div p #result-word");
+const charResult = document.querySelector(
+  ".main-container div p #result-character"
+);
+let len2 = 0;
+textareaEle.addEventListener("input", function () {
+  const inputString = textareaEle.value;
+
+  // This Code Will Give Me Word Count
+  // <!!!---!!!>
+  const trimmedInputString = inputString.trim();
+  const newStringArray = trimmedInputString.split(" ");
+  wordResult.innerHTML = newStringArray.length;
+  // <!!!---!!!>
+
+  // This Code Will Give Me Character Count
+  // <!!!---!!!>
+  const joinedString = newStringArray.join("");
+  charResult.innerHTML = joinedString.length;
+  // <!!!---!!!>
+
+  if (str == "") {
+    newStringArray.length = 0;
+    joinedString.length = 0;
+  }
+});
